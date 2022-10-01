53a75f499406425f820c5a9961306fd9c311235f
selimertoker <65356184+selimertoker@users.noreply.github.com>
2022-10-01 16:46:56 +0000

    Add files via upload

---
+++ ppm-lib.h
@@ -0,0 +1,9 @@
+
+bool help(int,char**);
+int sizeint(int);
+int* sizeimg(char*);
+int* readimg(char*);
+int writeimg(int*,int*,char*);
+int* newimg(int*);
+int setpxl(int*,int*,int,int,int*);
+int* getpxl(int*,int*,int,int);
